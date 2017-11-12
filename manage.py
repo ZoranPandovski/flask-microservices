6ca57b876d7f74fd7d7bc33441b5948e0eafde7f
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-11-13 00:32:33 +0100

    Add sql alchemy, add new db services

--- manage.py
+++ manage.py
@@ -1,7 +1,15 @@
 from flask_script import Manager
-from project import app
+from project import app, db
 
 manager = Manager(app)
+
+
+@manager.command
+def recreate_db():
+	db.drop_all()
+	db.create_all()
+	db.session.commit()
+
 
 if __name__ == '__main__':
 	manager.run()
--- project/__init__.py
+++ project/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify
 import os
+import datetime
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-#create app
+
+# instantiate the app
 app = Flask(__name__)
 
-#set config
+# set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
+
+# routes
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():

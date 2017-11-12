04b5500d36218662b4bebe3d2ce02a06da24ac4b
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-11-12 23:51:32 +0100

    Setup docker-compose

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
+import os
 
 #create app
 app = Flask(__name__)
 
 #set config
-app.config.from_object('project.config.DevelopmentConfig')
-
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
